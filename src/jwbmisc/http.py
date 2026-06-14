32fc9bc4ce6ebc882a97a153199a304a14bb69a0
Joachim Bargsten <jw@bargsten.org>
2026-06-14 14:58:53 +0200

    add bearer auth

---
+++ src/jwbmisc/http.py
@@ -0,0 +1,15 @@
+# Source - https://stackoverflow.com/a/58055668
+# Posted by Zhe, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-06-14, License - CC BY-SA 4.0
+
+import requests as rq
+
+class BearerAuth(rq.auth.AuthBase):
+    """auth=BearerAuth('s3cr3t')"""
+    token: str
+    def __init__(self, token):
+        super().__init__()
+        self.token = token
+    def __call__(self, req):
+        req.headers["Authorization"] = "Bearer " + self.token
+        return req
